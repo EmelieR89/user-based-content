885fe1c2da8b7063aa8dcf8e4577bf52b93f7157
Emelie Rosenlöw <rosenlowemelie@gmail.com>
2020-05-08 11:23:24 +0200

    First attempt to print users from database to frontend

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,36 +2,35 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
- const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
 
-/* const [recipes, setRecipe] = useState([]);
- */
+  /* const [recipes, setRecipe] = useState([]);
+   */
 
-/*   const getStuff = async () => {
+  const getStuff = async () => {
     await fetch("http://localhost:4000/api/users")
-      .then((response) => response.json())
-      .then((data) => {
-        setData(data);
-      });
-  }; */
-
-  const getRecipes = async () => {
-    await fetch("http://localhost:4000/api/recipes")
       .then((response) => response.json())
       .then((data) => {
         setData(data);
       });
   };
 
+  // const getRecipes = async () => {
+  //   await fetch("http://localhost:4000/api/recipes")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setData(data);
+  //     });
+  // };
 
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={() => getRecipes()}>tryck här!</button>
+        <button onClick={() => getStuff()}>tryck här!</button>
         {data.map((x, i) => (
           <div key={i}>
-            <h4>{x.title}</h4>
-            <p>{x.content}</p>
+            <h4>{x.name}</h4>
+            <p>{x.password}</p>
           </div>
         ))}
       </header>
