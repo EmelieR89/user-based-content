edabb1bb0b53ded675e3bba2e0165ccaaba14faa
Emelie Rosenlöw <rosenlowemelie@gmail.com>
2020-05-10 19:31:19 +0200

    Added

--- back-end/routes/userRoutes.js
+++ back-end/routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const UserModel = require("../models/userModel");
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
 const router = express.Router();
 
 // GET all users
@@ -34,109 +34,107 @@ router.post("/api/users", async function (req, res, next) {
   res.status(200).json(savedUserDoc); //send(JSON.stringify(savedUserDoc));
 });
 
-//DELETE user by id
-router.delete("/api/users/:userId", async function (req, res, next) {
-  const id = req.params.userId;
-  try {
-    const res = await UserModel.findByIdAndDelete(id);
-    res.status(200).send("Has been deleted");
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
+// //DELETE user by id
+// router.delete("/api/users/:userId", async function (req, res, next) {
+//   const id = req.params.userId;
+//   try {
+//     const res = await UserModel.findByIdAndDelete(id);
+//     res.status(200).send("Has been deleted");
+//   } catch (error) {
+//     res.status(400).send(error);
+//   }
+// });
 
 // POST signup
 router.post("/api/users/signup", async (req, res, next) => {
-  await UserModel.find({name: req.body.name})
-  .exec()
-  .then(user => {
-    if(user.length >= 1) {
-      return res.status(409).json({
-        message: "name already exists"
-      })
-    } else {
-      bcrypt.hash(req.body.password, 10, async (err, hash) => {
-        if(err) {
-          return res.status(500).json({
-            error: err
-          })
-        } else {
-          const user = new UserModel ({
-            _id: new mongoose.Types.ObjectId(), 
-            name: req.body.name,
-            password: hash
-          })
-          await user.save()
-          .then(result => {
-            console.log(result);
-            res.status(201).json({
-              message: "User created"
-            })
-          })
-          .catch(err => {
-            console.log(err);
-            res.status(500).json({
-              error: err
-            })
-          })
-        }
-      })
+  await UserModel.find({ name: req.body.name })
+    .exec()
+    .then((user) => {
+      if (user.length >= 1) {
+        return res.status(409).json({
+          message: "name already exists",
+        });
+      } else {
+        bcrypt.hash(req.body.password, 10, async (err, hash) => {
+          if (err) {
+            return res.status(500).json({
+              error: err,
+            });
+          } else {
+            const user = new UserModel({
+              _id: new mongoose.Types.ObjectId(),
+              name: req.body.name,
+              password: hash,
+            });
+            await user
+              .save()
+              .then((result) => {
+                console.log(result);
+                res.status(201).json({
+                  message: "User created",
+                });
+              })
+              .catch((err) => {
+                console.log(err);
+                res.status(500).json({
+                  error: err,
+                });
+              });
+          }
+        });
+      }
+    });
+});
 
-    }
-  })
-})
-
-// POST sign in 
-router.post("/api/users/login",  async (req, res, next) => {
-  await UserModel.find({name: req.body.name})
-  .exec()
-  .then(user => {
-    if(user.length < 1) {
-      return res.status(401).json({
-        message: "Auth failed"
-      })
-    }
-    bcrypt.compare(req.body.password, user[0].password, (err, result) => {
-      if (err) {
+// POST sign in
+router.post("/api/users/login", async (req, res, next) => {
+  await UserModel.find({ name: req.body.name })
+    .exec()
+    .then((user) => {
+      if (user.length < 1) {
         return res.status(401).json({
-          message: " Auth failed"
-        })
+          message: "Auth failed",
+        });
       }
-      if(result) {
-        return res.status(200).json({
-          message: "Auth successful"
-        })
-      }
-      res.status(401).json({
-        message: " Auth failed"
-      })
-    })  
-  })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json({
-          error: err
-        })
-        
-      })
-})
+      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+        if (err) {
+          return res.status(401).json({
+            message: " Auth failed",
+          });
+        }
+        if (result) {
+          return res.status(200).json({
+            message: "Auth successful",
+          });
+        }
+        res.status(401).json({
+          message: " Auth failed",
+        });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
 
 router.delete("/api/users/:userId", async function (req, res, next) {
-const id = req.params.userId
-  await UserModel.deleteOne({_id: id})
-  .exec()
-  .then(result => {
-    res.status(200).json({
-      message: "user deleted"
+  const id = req.params.userId;
+  await UserModel.deleteOne({ _id: id })
+    .exec()
+    .then((result) => {
+      res.status(200).json({
+        message: "user deleted",
+      });
     })
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json({
-      error:err
-    })
-    
-  })
-})
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
 
 module.exports = router;
